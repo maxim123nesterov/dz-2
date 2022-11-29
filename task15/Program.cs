1f28da18e6b3aa222e8639b4831ba7c3af4a2bc5
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-11-29 16:58:04 +0300

    задание 13

---
+++ task15/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+      Console.WriteLine("Не выходной");
+      break;
+    case 2:
+      Console.WriteLine("Не выходной");
+      break;
+    case 3:
+      Console.WriteLine("Не выходной");
+      break;
+    case 4:
+      Console.WriteLine("Не выходной");
+      break;
+    case 5:
+      Console.WriteLine("Не выходной");
+      break;
+    case 6:
+      Console.WriteLine("Выходной");
+      break;
+    case 7:
+      Console.WriteLine("Выходной");
+      break;      
+}
