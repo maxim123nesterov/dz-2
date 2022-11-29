15c8959f24f50b4da783f1b28137f00e4dae57c9
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-11-29 16:33:19 +0300

    Доделать 13 задание

---
+++ task13/Program.cs
@@ -0,0 +1,10 @@
+﻿//Нахождение 3 цифры трехзначного числа.
+Console.WriteLine("Введите число");
+string str = Console.ReadLine();
+Console.WriteLine(str[2]);
+//int B = number % 10;
+//Console.WriteLine(B);
+if (str[2] = 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
