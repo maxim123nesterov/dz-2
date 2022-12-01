8c51f91bbe4b0d66759d2f4e55269b7205c9f4ee
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-01 12:33:35 +0300

    задание 13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,10 +1,12 @@
 ﻿//Нахождение 3 цифры трехзначного числа.
 Console.WriteLine("Введите число");
-string str = Console.ReadLine();
-Console.WriteLine(str[2]);
-//int B = number % 10;
-//Console.WriteLine(B);
-if (str[2] = 0)
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 99 && a < 1000)
+{
+    int num = a % 10;
+    Console.Write($"третья цифра {num}");
+}
+if (a < 100)
 {
     Console.WriteLine("третьей цифры нет");
 }
